0d3c9c9ceb6fab8de0913b9547de6d92eead8f1d
Paul Scheunemann <paul.scheunemann@greenbone.net>
2024-12-04 11:06:41 +0100

    Change: Improve test usage
    The previous version of the test needed to be changed for every new
    version of postgres, now we are using variables to template most of the
    behavior.

--- tests/test-upgrade.sh
+++ tests/test-upgrade.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # This script is used to test the upgradeversion script without spinning up the
 # whole OpenSight stack. It creates a new (outdated) PostgreSQL database,
 # attempts to upgrade it and checks if the version file was updated correctly.
 
-set -euo pipefail
+DOCKER_IMAGE=packages.greenbone.net/opensight/opensight-postgres
+PREVIOUS_VERSION=16.6  # released version to test the upgrade from
 
 tempdir=$(mktemp -d -t opensight-postgres-test-XXXXX)
 
@@ -16,10 +18,23 @@ trap cleanup ERR
 sudo chown -R 999:999 "${tempdir}"
 
 # Start a PostgreSQL container with an outdated version
-docker run -it --rm --user 999:999 -e POSTGRES_PASSWORD=password -e POSTGRES_DB=keycloak -e POSTGRES_USER=keycloak -v "${tempdir}":/var/lib/postgresql/data packages.greenbone.net/opensight/opensight-postgres:16.6
+docker run -it --rm \
+  --user 999:999 \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_DB=keycloak \
+  -e POSTGRES_USER=keycloak \
+  -v "${tempdir}":/var/lib/postgresql/data \
+  ${DOCKER_IMAGE}:${PREVIOUS_VERSION}
 
-# Upgrade the database
-docker run -it --rm --user 999:999 -e POSTGRES_PASSWORD=password -e POSTGRES_DB=keycloak -e POSTGRES_USER=keycloak -v "${tempdir}":/var/lib/postgresql/data packages.greenbone.net/opensight/opensight-postgres:17.6 upgradeversion inplace
+# Upgrade the database with the test version
+# we can build a local testing image with the following command:
+#   docker build -t ${DOCKER_IMAGE}:test .
+docker run -it --rm \
+  --user 999:999 -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_DB=keycloak \
+  -e POSTGRES_USER=keycloak \
+  -v "${tempdir}":/var/lib/postgresql/data \
+  ${DOCKER_IMAGE}:test upgradeversion inplace
 
 # did everything work as expected?
 sudo cat ${tempdir}/PG_VERSION
